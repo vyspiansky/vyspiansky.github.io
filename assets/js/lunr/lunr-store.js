c7a3fd873df70294845768dd4e7e40a2c8df04b1
Ihor Vyspiansky <ivyspianskyi@n-ix.com>
2018-10-13 15:19:46 +0300

    Add a new article

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -33,4 +33,9 @@ var store = [{
         "excerpt":"$ echo $PATHOutput: /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin Create a symbolic link $ ln -s \"/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl\" /usr/local/bin/sublTo open the entire current directory $ subl .","categories": ["Mac"],
         "tags": ["command line","terminal","Sublime Text"],
         "url": "https://vyspiansky.github.io/2018/08/30/launch-sublime-text-3-terminal/",
+        "teaser":null},{
+        "title": "Error: tool 'xcodebuild' requires Xcode... on macOS",
+        "excerpt":"If you get the following error with npm install  xcode-select: error: tool 'xcodebuild' requires Xcode, but active developer directory'/Library/Developer/CommandLineTools' is a command line tools instancethen to fix it try (optional) Install Xcode - go to the AppStore and download the Xcode https://itunes.apple.com/us/app/xcode/id497799835 Point the xcode-select developer directory to the appropriate directory from within Xcode.app. To do this, runsudo xcode-select -s /Applications/Xcode.app/Contents/Developer...","categories": ["Mac"],
+        "tags": ["command line","terminal","Xcode"],
+        "url": "https://vyspiansky.github.io/2018/09/28/xcodebuild-requires-xcode/",
         "teaser":null}]
